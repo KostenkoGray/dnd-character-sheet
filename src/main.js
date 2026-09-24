4f95c4d4228509b830d9580b1dbe2a3b34f535d1
KostenkoGray <kostenkosergey2021@gmail.com>
2026-09-25 02:20:52 +0300

    Add persistent action preference handling

--- src/main.js
+++ src/main.js
@@ -15,7 +15,8 @@ import {
 import { charactersScreen } from "./screens/characterListScreen.js";
 import { characterSheetScreen } from "./screens/characterSheetScreen.js";
 import { combatScreen } from "./screens/combatScreen.js";
-import { saveCharacters } from "./services/storageService.js";
+import { saveCharacters, loadSettings, saveSettings } from "./services/storageService.js";
+import { DEFAULT_SETTINGS, ACTION_PREFERENCE_VALUES } from "./data/settingsData.js";
 import { bottomNavigation } from "./components/bottomNavigation.js";
 import {
   REST_TYPES,
@@ -33,6 +34,18 @@ const app = document.querySelector("#app");
 
 let currentCharacter = null;
 let currentScreen = "list";
+
+const settings = loadSettings(DEFAULT_SETTINGS);
+
+function getActionPreference(key) {
+  return settings.actionPreferences?.[key] ?? null;
+}
+
+function setActionPreference(key, value) {
+  settings.actionPreferences ??= {};
+  settings.actionPreferences[key] = value;
+  saveSettings(settings);
+}
 
 function navigationForCurrentScreen() {
   return bottomNavigation(currentScreen);
