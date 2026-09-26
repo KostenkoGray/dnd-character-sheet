863c35a32c1aa7d5f29cb187464aa22b29c2b12a
KostenkoGray <kostenkosergey2021@gmail.com>
2026-09-26 22:28:55 +0300

    use shared hit dice calculation

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import {
 import { charactersScreen } from "./screens/characterListScreen.js";
 import { characterSheetScreen } from "./screens/characterSheetScreen.js";
 import { combatScreen } from "./screens/combatScreen.js";
-import { getStatModifier } from "./services/characterCalculationsService.js";
+import { getStatModifier, getHitDiceTotal } from "./services/characterCalculationsService.js";
 import { CLASSES } from "./data/classesData.js";
 import { saveCharacters, loadSettings, saveSettings } from "./services/storageService.js";
 import { DEFAULT_SETTINGS, ACTION_PREFERENCE_VALUES } from "./data/settingsData.js";
